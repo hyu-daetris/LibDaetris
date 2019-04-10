f75370095cfa78113d5262e3d4512a65e8e35d80
revsic <revsic99@gmail.com>
2019-04-10 20:35:29 +0900

    serial: Replace template to callback_t

--- LibDaetris/Impl/Serial.hpp
+++ LibDaetris/Impl/Serial.hpp
@@ -3,14 +3,16 @@
 
 #include "ofMain.h"
 
-template <typename F>
+namespace LibDaetris
+{
 class SerialApp : public ofBaseApp {
 public:
-    template <typename Fp>
-    SerialApp(std::string port_name, int baud, Fp&& callback) : 
+    using callback_t = void(*)(int);
+
+    SerialApp(std::string port_name, int baud, callback_t callback) : 
         m_port_name(port_name),
         m_baud(baud),
-        m_callback(std::forward<Fp>(callback))
+        m_callback(callback)
     {
         // Do Nothing
     }
@@ -34,8 +36,9 @@ private:
     std::string m_port_name;
     int m_baud;
 
-    F m_callback;
+    callback_t m_callback;
     ofSerial m_serial;
 };
+}
 
 #endif
